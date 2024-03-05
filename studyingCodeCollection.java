f28b15a24b20dde873f4f22c5c8a9ffd7dd6bd29
Akim <rlawoals623@gmail.com>
2024-03-05 20:26:59 +0900

    Update studyingCodeCollection.java

--- studyingCodeCollection.java
+++ studyingCodeCollection.java
@@ -1,4 +1,32 @@
-  public int solution(String[] spell, String[] dic) {
+
+    // 직사각형 넓이 구하기
+    public int solution(int[][] dots) {
+        int answer = 0;
+        int tmp = dots[0][0];
+        int tmp2 = 0;
+        int tmp3 = 0;
+        for(int i = 1; i<dots.length; i++){
+            if(dots[i][0] == tmp){
+                tmp2 = Math.abs(dots[i][1] - dots[0][1]);
+            }else{
+                tmp3 = Math.abs(dots[i][0] - dots[0][0]);
+            }
+        }
+        answer = tmp2 * tmp3;
+        return answer;
+    }
+    public int solution(int[][] dots) {
+        int x = Math.max(Math.abs(dots[0][0] - dots[1][0]),Math.abs(dots[0][0] - dots[2][0]));
+        int y = Math.max(Math.abs(dots[0][1] - dots[1][1]),Math.abs(dots[0][1] - dots[2][1]));
+        return x*y;
+    }
+    public int solution(int[][] dots) {
+        Arrays.sort(dots,Comparator.comparing((int[] it)-> it[0]).thenComparing((int[] it)->it[1]));
+        return (dots[3][0] - dots[0][0]) * (dots[3][1] - dots[0][1]);
+    }
+
+    // 
+    public int solution(String[] spell, String[] dic) {
         return Arrays.stream(dic).
         map(s -> 
         s.chars().sorted().mapToObj(i -> String.valueOf((char) i)).collect(Collectors.joining())
