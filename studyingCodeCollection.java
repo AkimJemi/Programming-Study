d905851ef47c7a8339a80597667b56498f99fe8d
Akim <rlawoals623@gmail.com>
2024-03-07 20:21:40 +0900

    Update studyingCodeCollection.java

--- studyingCodeCollection.java
+++ studyingCodeCollection.java
@@ -1,3 +1,24 @@
+
+
+    // 등수 매기기
+    public int[] solution(int[][] score) {
+        List<Integer> scoreList = new ArrayList<>();
+        for(int[] t : score){
+            scoreList.add(t[0] + t[1]);
+        }
+        scoreList.sort(Comparator.reverseOrder());
+
+        int[] answer = new int[score.length];
+        for(int i=0; i<score.length; i++){
+            answer[i] = scoreList.indexOf(score[i][0] + score[i][1])+1;
+        }
+        return answer;
+    }
+
+    public int[] solution(int[][] score) {
+        return Arrays.stream(score).map(ints -> Arrays.stream(ints).average().orElse(0)).mapToInt(d -> Arrays.stream(score).map(ints -> Arrays.stream(ints).average().orElse(0)).sorted(Comparator.reverseOrder()).collect(Collectors.toList()).indexOf(d) + 1).toArray();
+    }
+
     // 로그인 성공?
     public String solution(String[] idPw, String[][] db) {
         return getDb(db).get(idPw[0]) != null ? getDb(db).get(idPw[0]).equals(idPw[1]) ? "login" : "wrong pw" : "fail";
