ffb7c59d348bea7538227423c08d3cd15c92b765
Akim <rlawoals623@gmail.com>
2024-03-09 17:22:10 +0900

    Update studyingCodeCollection.java

--- studyingCodeCollection.java
+++ studyingCodeCollection.java
@@ -1,3 +1,49 @@
+
+    // 안전지대
+    public int solution(int[][] board) {
+        int answer = 0;
+        for (int i=0; i<board.length; i++) {
+            for (int j=0; j<board[0].length; j++) {
+                if (i-1 >= 0 && j-1 >= 0 && board[i-1][j-1] == 1) { continue; }
+                if (j-1 >= 0 && board[i][j-1] == 1) { continue; }
+                if (i+1 <= board.length-1 && j-1 >= 0 && board[i+1][j-1] == 1) { continue; }
+                if (i-1 >= 0 && board[i-1][j] == 1) { continue; }
+                if (board[i][j] == 1) { continue; }
+                if (i+1 <= board.length-1 && board[i+1][j] == 1) { continue; }
+                if (i-1 >= 0 && j+1 <= board.length-1 && board[i-1][j+1] == 1) { continue; }
+                if (j+1 <= board.length-1 && board[i][j+1] == 1) { continue; }
+                if (i+1 <= board.length-1 && j+1 <= board.length-1 && board[i+1][j+1] == 1) { continue; }
+                answer++;
+            }
+        }
+        return answer;
+    }
+    @FunctionalInterface
+    interface BiConsumer<T> {
+        void apply(Integer t1, Integer t2);
+    }
+    public int solution(int[][] board) {
+        int answer;
+        int[] move = {-1,0,1};
+        int[][] chkBoard = new int[board.length][board.length];
+
+        BiConsumer<Integer> drawChk = (x, y) ->{
+            Arrays.stream(move)
+                    .filter((i)-> 0<= x+i && x+i < board.length)
+                    .forEach(i->Arrays.stream(move)
+                            .filter(j-> 0 <= j+y && j+y < board.length)
+                            .forEach((j)->chkBoard[x+i][y+j] = 1));
+        };
+
+        IntStream.range(0, board.length).forEach(i -> IntStream.range(0, board.length).filter(j->board[i][j] == 1).forEach(j->drawChk.apply(i,j)));
+
+        answer = (int)Arrays.stream(chkBoard).mapToLong(i -> Arrays.stream(i).filter(j -> j == 0).count()).sum();
+
+        System.out.println();
+
+        return answer;
+    }
+
     // 문자열 밀기
     public int solution(String A, String B) {
         String tempB = B.repeat(3);
