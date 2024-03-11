d64818a198e9fa509ecaed7d2d487a2a725abe72
Akim <rlawoals623@gmail.com>
2024-03-11 22:19:51 +0900

    Create LeetCodeMe.java

---
+++ LeetCodeMe.java
@@ -0,0 +1,47 @@
+
+
+
+// 2. Add Two Numbers
+// Medium
+import java.math.BigInteger;
+import java.util.Arrays;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        StringBuilder l1StrBuilder = new StringBuilder();
+        while (true) {
+            l1StrBuilder.append(l1.val);
+            if (l1.next == null) {
+                break;
+            }
+            l1 = l1.next;
+        }
+        StringBuilder l2StrBuilder = new StringBuilder();
+        while (true) {
+            l2StrBuilder.append(l2.val);
+            if (l2.next == null) {
+                break;
+            }
+            l2 = l2.next;
+        }
+        String bigInteger = new BigInteger(l1StrBuilder.reverse().toString()).
+                add(new BigInteger(l2StrBuilder.reverse().toString())).toString();
+        String[] strArray = new StringBuilder().append(bigInteger)
+                .toString().split("");
+        ListNode resultNode = Arrays.stream(strArray).mapToInt(Integer::parseInt).mapToObj(a -> new ListNode(a)).reduce((a, b) -> {
+                    b.next = a;
+                    return b;
+                }
+        ).get();
+        return resultNode;    
+    }
+}
